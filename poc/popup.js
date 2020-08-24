160a9d0176634d98e3b7d5e40f875541e4b2dbeb
prtksengar3 <prtksengar3@gmail.com>
2020-08-24 19:45:27 +0530

    added button

--- poc/popup.js
+++ poc/popup.js
@@ -1,1 +1,8 @@
-console.log("")
+// console.log("");
+const button = document.querySelector("button");
+button.addEventListener("click",function(){
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        console.log(tabs);
+        chrome.tabs.sendMessage(tabs[0].id,"message from popup")
+    })
+})
